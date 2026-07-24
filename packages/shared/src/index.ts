1c5634bd0acbdb04326208e77d44de47c8a68a35
nhn141 <142210395+nhn141@users.noreply.github.com>
2026-07-24 22:37:41 +0700

    Initial commit

---
+++ packages/shared/src/index.ts
@@ -0,0 +1,8 @@
+export enum GamePhase {
+    LOBBY = 'LOBBY',
+    ROLE_REVEAL = 'ROLE_REVEAL',
+    DRAWING = 'DRAWING',
+    VOTING = 'VOTING',
+    FAKE_GUESSING = 'FAKE_GUESSING',
+    RESULT = 'RESULT',
+}
